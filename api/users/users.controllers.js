2a9e1139ae2cd5636e7c72afc5f5a811120219f9
mohammadbq <iiprxh@gmail.com>
2023-06-22 18:17:25 +0300

    done

--- api/users/users.controllers.js
+++ api/users/users.controllers.js
@@ -1,5 +1,38 @@
 const User = require('../../models/User');
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+require("dotenv").config();
 
+const hashPassword = async (password) => {
+  const saltRounds = 10
+  const hashedPassword = await bcrypt.hash(password,saltRounds)
+  return hashedPassword
+}
+
+const createToken = (user)  =>{
+  const payload = {
+    _id : user._id,
+    username : user.username
+  }
+  const token = jwt.sign(payload,process.env.JWT_SECRET, {expiresIn :"1h"});
+  return token
+}
+
+
+
+
+exports.signup =  async (req,res,next) =>{
+  try {
+    const { password } = req.body
+    req.body.passwors = await hashPassword(password)
+    const newUser = await User.create(req.body)
+    const token = createToken(newUser)
+    return res.status(201).json(token)
+  } catch (error) {
+    return next(error)
+  }
+  
+}
 exports.signin = async (req, res) => {
   try {
   } catch (err) {
